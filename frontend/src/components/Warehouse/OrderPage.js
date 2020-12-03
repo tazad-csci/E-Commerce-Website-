621ad463be58736964c9fb2fd42780e085994e92
TristenAnderson <tristendanderson@gmail.com>
2020-12-02 22:39:35 -0600

    react router added

---
+++ frontend/src/components/Warehouse/OrderPage.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class OrderPage extends React.Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+    
+        }       
+    }
+
+
+    render() {
+        return(
+            <div>
+               Hello There
+            </div>
+        )
+    }
+}
+
+export default OrderPage
