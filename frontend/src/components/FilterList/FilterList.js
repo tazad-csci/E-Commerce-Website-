b8ce589cb2a2a05d6c6224c404f764e1cc984fd3
dmratcliffe <dmrat@dmratcliffe.com>
2020-12-02 22:27:18 -0600

    filter list to website header

--- frontend/src/components/FilterList/FilterList.js
+++ frontend/src/components/FilterList/FilterList.js
@@ -8,12 +8,25 @@ export default function FilterList(props) {
     return (
         <div className="filter-bar">
             <span className="website-header"> Parts Buyer </span>
-            <input className="input" type="text"
+            {
+                !props.name ? 
+                ( 
+                    <input className="input" type="text"
                 placeholder="Search for a part..."
                 value={props.cur_filter}
                 onChange={e => props.change_filter(e.target.value)}
 
             />
+                )
+
+                :
+                (
+                    <span className="website-header">
+                        {props.name}
+                    </span>
+                )
+            }
+            
         </div>
 
     )
